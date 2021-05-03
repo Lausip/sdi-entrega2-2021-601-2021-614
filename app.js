b8553afb0d124654ddac3b31a1cd50807cf346d7
uo271432 <uo271432@uniovi.es>
2021-05-03 20:18:45 +0200

    W1-Regsitro_V1.0

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // Módulos
 let express = require('express');
 let app = express();
-
+//REST
 let rest = require('request');
 app.set('rest',rest);
 
+// Log
+let log4js = require('log4js');
+log4js.configure({
+    appenders: {myWallapop: {type: 'file', filename: 'logs/myWallapop.log'}},
+    categories: {default: {appenders: ['myWallapop'], level: 'trace'}}
+});
+let logger = log4js.getLogger('myWallapop');
+app.set('logger', logger);
+
+//JQUERY
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
@@ -42,10 +52,14 @@ let gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app,mongo);
 
 //Variables
-//.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.nq3br.mongodb.net:27017,tiendamusica-shard-00-01.nq3br.mongodb.net:27017,tiendamusica-shard-00-02.nq3br.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-tdx28j-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.nq3br.mongodb.net:27017,tiendamusica-shard-00-01.nq3br.mongodb.net:27017,tiendamusica-shard-00-02.nq3br.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-tdx28j-shard-0&authSource=admin&retryWrites=true&w=majority');
 app.set('clave','abcdefg');
 app.set('crypto',crypto);
 
+//Rutas controladores
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+
+//Puerto
 app.set('port',8081);
 app.listen(app.get('port'), function () {
     console.log("Servidor activo");
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,3 +1,67 @@
 module.exports =function (app,swig,gestorBD){
 
+    /**
+     * Muestra la página de registro si hay usuario en sesión,
+     * si no lo hay vuelve al home
+     */
+    app.get("/registrar", function (req, res) {
+        if (req.session.usuario === undefined) {
+        let respuesta = swig.renderFile('views/bregistro.html', {});
+        app.get('logger').info('Registrarse: se va a mostrar la página de registro');
+        res.send(respuesta);}
+        else{
+            app.get('logger').error('Registrarse: no estas en sesión')
+            res.redirect("/home");
+        }
+    });
+    /**
+     * Permite el registro de un usuario si todo va bien
+     * si no se explica el error
+     */
+    app.post('/usuario', function (req, res) {
+        app.get('logger').info('Usuario se va a registrar');
+        if (req.body.nombre === undefined || req.body.nombre === '' || req.body.apellidos === undefined || req.body.apellidos === ''
+            || req.body.password === undefined || req.body.password === '' || req.body.repassword === undefined || req.body.repassword === ''
+            || req.body.email === undefined || req.body.email === '') {
+            res.redirect("/registrarse?mensaje=Es necesario completar todos los campos&tipoMensaje=alert-danger")
+            app.get('logger').error('Es necesario completar todos los campos');
+        } else if (req.body.password !== req.body.repassword) {
+            res.redirect("/registrarse?mensaje=Las contraseñas deben ser iguales&tipoMensaje=alert-danger")
+            app.get('logger').error('Las contraseñas deben ser iguales');
+        } else {
+            let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let usuario = {
+                email: req.body.email,
+                nombre: req.body.name,
+                apellido: req.body.surname,
+                dinero: 100,
+                rol: 'rol_estandar',
+                password: seguro
+            };
+            let criterio = {
+                email: req.body.email
+            };
+            gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                if (usuarios != null && usuarios.length !== 0) {
+                    app.get("logger").error('El email ya está registrado');
+                    res.redirect("/registrarse?mensaje=El email ya está registrado&tipoMensaje=alert-danger");
+                } else {
+                    gestorBD.insertarUsuario(usuario, function (id) {
+                        if (id === undefined) {
+                            app.get("logger").error('Error al registrar al usuarios');
+                            res.redirect("/registrarse?mensaje=Error al registrar usuario&tipoMensaje=alert-danger")
+                        } else {
+                            req.session.usuario = usuario;
+                            app.get("logger").info('Usuario registrado como ' + req.body.email);
+                            delete req.session.usuario.password;
+                            res.redirect("/home");
+                        }
+                    })
+                }
+            })
+        }
+    });
+
+
 }
