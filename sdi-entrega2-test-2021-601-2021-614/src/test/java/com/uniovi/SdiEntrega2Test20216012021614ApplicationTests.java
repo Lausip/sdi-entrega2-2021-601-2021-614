4ecdc2341ac66d7c86c8f0dbb5781536805e526e
uo271432 <uo271432@uniovi.es>
2021-05-07 03:09:53 +0200

    Proyecto-test

---
+++ sdi-entrega2-test-2021-601-2021-614/src/test/java/com/uniovi/SdiEntrega2Test20216012021614ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega2Test20216012021614ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
